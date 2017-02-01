c7010831f39fdf7399290bee605768f6d722627b
Andre-PC\Andre <andre.pieterse.clix@gmail.com>
2017-02-01 22:37:00 +0200

    Fix DTO missing attribute that broke the services.

--- Server/Source/CLog.Services.Models/DtoBase.cs
+++ Server/Source/CLog.Services.Models/DtoBase.cs
@@ -1,8 +1,11 @@
-﻿namespace CLog.Services.Models
+﻿using System.Runtime.Serialization;
+
+namespace CLog.Services.Models
 {
     /// <summary>
     /// Represents the base data transfer object.
     /// </summary>
+    [DataContract]
     public abstract class DtoBase
     {
     }
