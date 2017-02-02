3569c00ed84dc2efd9585bbb60bc6176c8110c61
Andre-PC\Andre <andre.pieterse.clix@gmail.com>
2017-02-02 22:23:17 +0200

    Styling of boxes.

--- Client/Tests/CLog.UI.Testing.Configuration/DataHelpers/TimesheetsDataHelper.cs
+++ Client/Tests/CLog.UI.Testing.Configuration/DataHelpers/TimesheetsDataHelper.cs
@@ -12,7 +12,7 @@ namespace CLog.UI.Testing.Configuration.DataHelpers
 
             CapturedTimeDto[] items = new[]
             {
-                new CapturedTimeDto("UserName", (fromDate = fromDate.AddDays(1)), 8, true, false, true),
+                new CapturedTimeDto("UserName", (fromDate = fromDate.AddDays(1)), 8, true, false, false),
                 new CapturedTimeDto("UserName", (fromDate = fromDate.AddDays(1)), 7, false, true, true),
                 new CapturedTimeDto("UserName", (fromDate = fromDate.AddDays(1)), 6, false, false, false),
                 new CapturedTimeDto("UserName", (fromDate = fromDate.AddDays(1)), 5, false, false, true),
